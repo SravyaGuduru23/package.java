a43a5292da203c21ef875f40a566a11677273c5f
SravyaGuduru23 <31911350+SravyaGuduru23@users.noreply.github.com>
2017-09-13 12:42:45 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		StudentGroup sg= new StudentGroup(2);
+		Student[] s=new Student[2];
+		Student s1;
+		for(int i=0;i<s.length;i++)
+			s[i]=
+	}
+
+}
